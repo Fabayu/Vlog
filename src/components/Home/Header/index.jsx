11735ed0e51553feb9a492db02004d6baca1b162
Ayush <87146546+Fabayu@users.noreply.github.com>
2023-05-21 11:03:08 +0530

    Add files via upload

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Supercharge Your Skills: The Ultimate Development Blog App
+</h2>
+    <h1>
+      <span>“</span> Vlogs<span>”</span>
+    </h1>
+    <p>
+      Awesome place to make yourself <br /> productive and entertained through
+      daily updates.So stay tuned .
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,188 @@
+export const blogList = [
+  {
+
+
+
+
+    id: 1,
+    title: 'Hooks',
+    category: 'ReactJs',
+    subCategory: ['useState', 'useReducer', 'Hooks'],
+
+
+
+
+    description:
+
+"ReactJS hooks revolutionized the way developers manage state and lifecycle in React components, offering a concise and elegant solution. With useState, developers can effortlessly manage component-level state, enabling dynamic updates and rendering. useEffect allows for easy integration of side effects, such as fetching data or subscribing to events, ensuring efficient and clean code. useContext simplifies the sharing of data between components by providing a centralized state management solution. useCallback optimizes performance by memoizing functions, preventing unnecessary re-renders. useMemo enhances performance by caching expensive computations, reducing computational overhead. useRef facilitates direct access to DOM elements and persistent values across renders. useReducer offers an alternative to useState, allowing for more complex state management through a reducer function. useLayoutEffect mirrors useEffect but executes synchronously after all DOM mutations, ensuring accurate measurements and visual updates. useImperativeHandle grants control over instance values and methods exposed by custom components. useDebugValue aids in custom hook development by providing additional debugging information. useMutationEffect, despite being deprecated, shares similarities with useEffect and useLayoutEffect",   
+    authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Hooks.jpg',
+  },
+
+
+
+
+
+  {
+    id:5,
+    title: 'Javascript is Everywhere',
+    category: 'Javascript',
+    subCategory: ['Javascript', 'Language', 'Asynchronous Language'],
+    description:
+
+
+
+
+
+"JavaScript is everywhere, permeating the digital landscape like never before. From websites to mobile applications, it powers interactive experiences that captivate users worldwide. Its versatility enables developers to bring life to static web pages, making them dynamic and responsive. With the rise of front-end frameworks like React and Angular, JavaScript has become the go-to language for building modern, feature-rich interfaces. Moreover, JavaScript's reach extends beyond the browser, as it now finds applications in server-side programming through platforms such as Node.js. The proliferation of IoT devices further showcases JavaScript's ubiquity, allowing developers to control and automate smart devices seamlessly. Additionally, JavaScript's integration with popular content management systems like WordPress enables effortless customization and extensibility. With the advent of progressive web apps, JavaScript facilitates the creation of app-like experiences that work seamlessly across various devices. As a client-side scripting language, JavaScript is instrumental in enhancing user interactions, validating form inputs, and creating dynamic content on the fly",
+
+    authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/javascr.jpg',
+  },
+
+  
+
+{
+  id: 3,
+  title: 'Callback Hell',
+  category: 'Javascript',
+
+  subCategory: ['Pyramid of Doom', 'Javascript', 'Callback Hell'],
+  description:
+
+
+"Callback hell in JavaScript refers to a situation where multiple nested callbacks make code difficult to read, understand, and maintain. It occurs when asynchronous operations depend on each other, resulting in a pyramid of callback functions. Each nested callback further complicates the code flow, making it prone to errors and difficult to debug. The lack of a linear and sequential structure in callback hell hampers code readability and leads to what is known as Icallback spaghetti. Furthermore, error handling becomes challenging, as exceptions thrown in nested callbacks must be handled at each level. The introduction of Promises and async/await in JavaScript provides more elegant alternatives to mitigate callback hell by offering a more linear and concise syntax for handling asynchronous operations. By using Promises and async/await, developers can flatten their code structure, improving readability and making it easier to handle errors.",
+  
+  
+ 
+ 
+
+    
+    authorName: 'Ayush Jaiswal',
+  authorAvatar: '/assets/images/author.jpeg',
+  createdAt: 'June 03, 2021',
+  cover:
+    '/assets/images/Callback.jpeg',
+},
+    
+  {
+    id: 4,
+    title: 'Chatgpt vs Bard',
+    category: 'Trending',
+
+
+
+    subCategory: ['AI and AI', 'Bard', 'Chatgpt'],
+    description:
+
+"ChatGPT and BARD are two advanced language models developed by OpenAI. ChatGPT, based on GPT-3.5 architecture, excels in generating human-like responses and carrying on engaging conversations. It has been trained on a wide range of internet text to provide versatile and knowledgeable interactions. On the other hand, BARD, based on the Codex model, focuses on assisting developers with programming-related tasks. BARD offers code completions, explanations, and syntactic transformations, making it a valuable tool for software development. While both models share a common foundation, their specialized training and fine-tuning make them optimized for different use cases, with ChatGPT focusing on conversational abilities and BARD targeting programming assistance. The choice between the two depends on the specific needs of the user, whether it's seeking natural language conversation or programming support.",
+
+
+      
+      authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/bard.jpeg',
+    
+    
+  },
+
+
+
+
+  {
+    id: 5,
+    title: 'Material UI -ReactJs Framework',
+    category: 'ReactJs',
+    subCategory: ['Reactjs', 'MaterialUI', 'React Css'],
+    description:
+  
+  
+  
+  
+  
+      "Material-UI is a feature-rich UI component library for ReactJS, based on Material Design principles, providing pre-built and customizable components, a responsive grid system, theming capabilities, seamless ReactJS integration, and support for hooks and utilities, making it an ideal choice for building beautiful and interactive user interfaces. Its extensive collection of components includes buttons, cards, forms, and more, enabling developers to create visually appealing and consistent designs. With Material-UI, developers can easily customize the look and feel of their applications, ensure responsiveness across devices, and leverage React's component-based architecture for efficient development.",
+      authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/materialui.jpeg',
+  },
+
+
+
+
+
+
+
+
+  {
+  id: 6,
+    title: 'Leetcode and DSA',
+    category: ' Trending',
+    subCategory: ['Interviews', 'DSA', 'Leetcode'],
+    description:
+
+
+
+
+
+      "LeetCode and Data Structures and Algorithms (DSA) are crucial for interview preparation, helping candidates develop problem-solving skills, algorithmic thinking, and coding efficiency, which are highly valued by employers during technical interviews. By practicing LeetCode and mastering DSA concepts, candidates gain the ability to approach complex problems, optimize solutions, and demonstrate strong coding abilities in a limited time frame, increasing their chances of success in interviews. These platforms enable candidates to familiarize themselves with common interview questions, algorithms, and data structures, preparing them to tackle a variety of technical challenges and enhancing their problem-solving aptitude. LeetCode and DSA practice build confidence, improve algorithmic analysis skills, and provide a solid foundation for solving real-world programming problems, ultimately boosting interview performance and increasing the likelihood of securing job offers in the competitive tech industry.",
+    authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/LEET.jpeg',
+  },
+  
+  
+
+
+
+    
+  {
+    id: 7,
+    title: 'Reactjs vs Angular',
+    category: 'Trending',
+    subCategory: ['Angular', 'Reactjs', 'Comparison'],
+    description:
+ "ReactJS and Angular are both powerful frameworks for building web applications, but they have distinct approaches and features. ReactJS is a JavaScript library that emphasizes component-based development, allowing developers to build reusable UI components. It provides a flexible and lightweight solution, making it suitable for smaller projects or when you want more control over the application structure. Angular, developed by Google, is a comprehensive framework that offers a complete solution for building large-scale applications. It includes features like dependency injection, routing, form handling, and state management out-of-the-box, which can enhance productivity for complex projects. Angular follows a more opinionated approach with a defined project structure and strict coding conventions. The choice between ReactJS and Angular often depends on project requirements, developer preferences, and the size and complexity of the application.",
+      authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/angular.jpeg',
+  },
+
+  {
+    id: 8,
+    title: 'Router in Reactjs',
+    category: 'Recatjs',
+    subCategory: ['router', 'design', 'react'],
+    description:
+
+
+"A router in the context of web development is a crucial component that enables navigation and routing within a web application. It acts as a traffic controller, directing users to different pages or views based on the requested URL. Routers handle client-side routing, allowing for a seamless and dynamic user experience. By defining routes and their corresponding components, a router facilitates the rendering and updating of specific content based on the user's actions. It intercepts URL changes, matches them to defined routes, and triggers the appropriate component rendering or behavior. Routers often support features like nested routes, route parameters, and query parameters, allowing for flexible and customizable navigation. They enable bookmarkable and shareable URLs, ensuring that users can directly access specific pages or states within the application. Routers play a pivotal role in Single-Page Applications (SPAs) where the content dynamically updates without a full page reload. They provide a foundation for building complex applications with multiple views and help maintain a structured codebase by separating concerns related to routing and navigatio",
+      authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/router.jpg',
+  },
+
+  {
+    id: 9,
+    title: 'Promises',
+    category: 'Javascript',
+    subCategory: ['Promises', 'Promise chain', 'Javascript'],
+    description:
+
+
+
+"Promises in JavaScript provide a way to handle asynchronous operations. They have three states: pending, fulfilled, or rejected. Created with new Promise(), they take a function with resolve and reject parameters. Promises enable chaining using .then() and error handling with .catch(). Methods like .all() and .race() handle multiple promises simultaneously. .finally() allows for cleanup actions regardless of the promise outcome. Promises can be awaited using async/await syntax. They offer a structured approach to asynchronous programming, improving code readability and maintainability. Promises have become an essential feature in JavaScript for managing asynchronous tasks effectively.",
+      authorName: 'Ayush Jaiswal',
+    authorAvatar: '/assets/images/author.jpeg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/promises.jpeg',
+  },
+];
